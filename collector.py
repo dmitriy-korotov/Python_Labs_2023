9576fbfdddf74515de881dab2a94c9c332199ef7
Dmitriy <dmitriy.korotov.76@mail.ru>
2023-11-16 21:40:48 +0300

    Added logging in collector

--- collector.py
+++ collector.py
@@ -3,9 +3,29 @@ import email
 from utility import load_settings
 import time
 import threading
+import re
 
 
 APPLICATION_IS_RUNNING = True
+
+
+def log_message(_msg: str, _file: str) -> None:
+    with open(_file, "a", encoding="utf-8") as log:
+        log.write(f"=> {_msg}")
+
+
+def is_valid_subject(_subject: str) -> bool:
+    #pattern = r"^<[Ticket #[1-9]*] Mailer>"
+    #re.match(pattern, _subject)
+    return False
+
+
+def get_id_from_subject(_subject: str) -> str:
+    ID = ""
+    start = _subject.find('#') + 1
+    while _subject[start].isdigit():
+        ID += _subject[start]
+    return ID
 
 
 def letters_handler(_settings: dict) -> None:
@@ -24,18 +44,22 @@ def letters_handler(_settings: dict) -> None:
         while True:
             print("=> Email checked")
 
-            status, uid_list_str = connection.uid("search", "UNSEEN", "ALL")
+            status, uid_list_str = connection.uid("search", "ALL")
             print(uid_list_str)
             if status != "OK":
                 print("=> ERROR: Can't get access to letters uid")
                 continue
 
-            uid_list = tuple(map(int, uid_list_str[2: len(uid_list_str) - 1]))
+            uid_list = tuple(map(int, uid_list_str[0][2: len(uid_list_str[0]) - 1].split()))
             for uid in uid_list:
                 res, message = connection.uid("fetch", str(uid), "(RFC822)")
                 message = email.message_from_bytes(message[0][1])
                 subject = message["Subject"]
                 print(subject)
+                if is_valid_subject(subject):
+                    log_message(f"[SUCCESS] ID: {get_id_from_subject(subject)} => message: \n", _settings["SUCCESS_LOG"])
+                else:
+                    log_message(f"[ERROR] Message: \n", _settings["ERROR_LOG"])
 
             time.sleep(period_check)
         connection.close()
